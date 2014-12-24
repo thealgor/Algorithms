5540d75ab26478ca72de18aa122a9320923b0fdd
Sunil <sunil.kksubramanyam@salesforce.com>
2014-12-23 23:58:04 -0800

    Merge Sort/Inversions

---
+++ Basic/sorting/CountingInversions.java
@@ -0,0 +1,31 @@
+public class CountingInversions{
+
+   public static void count(Comparable[] arr, Comparable[] aux, int lo, int hi){
+      long inversions = 0;
+      if(hi<=lo) return;
+      int mid = lo + (hi-lo)/2;
+      inversions += count(arr,aux,lo,mid);
+      inversions += count(arr,aux,mid+1,hi);
+      inversions += merge(arr,aux,lo,mid,hi);
+      return inversions;
+   }
+   
+   // | | | | | | | | i        mid  mid-i+1 elements are greater than arr[j] +1 is for including arr[i] also
+
+   public void merge(Comaparable[] arr, Comparable[] aux, int lo, int mid, int hi){
+     int i = lo, j=mid+1;
+     long inversions = 0;
+     for(int k=lo;k<=hi;k++)
+     	aux[k]=arr[k];
+     for(int k=lo;k<=hi;k++){
+     	if(i>mid)arr[k]=aux[j++];
+     	else if(j>hi) arr[k]=aux[i++];
+     	else if(aux[j]<aux[i]){arr[k]=aux[j++]; inversions+=(mid-i)+1;}
+     	else arr[k]=aux[i++];
+     }
+
+   }
+
+}
+
+
---
+++ Basic/sorting/MergeSort.java
@@ -0,0 +1,57 @@
+public class Arrays{
+   
+  public static void sort(Comparable[] arr){
+	Comparable[] aux = new Comparable[arr.length];
+  	sort(arr,aux,0,arr.length-1);
+  }
+
+  public static void sort(Comparable[] arr. Comparable[] aux, int lo, int hi){
+     if(hi<=lo) return;
+     int mid = lo+(hi-lo)/2;
+     sort(arr,aux,lo,mid);
+     sort(arr,aux,mid+1,hi);
+     if(arr[mid]>arr[mid+1])
+     	merge(arr,aux,lo,mid,hi);
+  }
+  
+
+  public void merge(Comparable[] arr, Comparable[] aux, int lo, int mid, int hi){
+     int i = lo, j=mid+1;
+     for(int k=lo;k<=hi;k++)
+       aux[k]=arr[k];
+
+     for(int k=lo;k<=hi;k++){
+         if(i>mid) arr[k]=aux[j++];
+         else if(j>hi) arr[k]=aux[i++];
+         else if(aux[j]<aux[i]) arr[k]=aux[j++];
+         else arr[k]=aux[i++];
+	} 
+
+  }
+
+  //without one extra copy
+
+   public static void sort(Comparable[] arr. Comparable[] aux, int lo, int hi){
+     if(hi<=lo) return;
+     int mid = lo+(hi-lo)/2;
+     sort(aux,arr,lo,mid);
+     sort(aux,arr,mid+1,hi);
+     if(arr[mid]>arr[mid+1])
+     	merge(aux,arr,lo,mid,hi);
+  }
+  
+
+  public void merge(Comparable[] arr, Comparable[] aux, int lo, int mid, int hi){
+     int i = lo, j=mid+1;
+   
+     for(int k=lo;k<=hi;k++){
+         if(i>mid) aux[k]=arr[j++];
+         else if(j>hi) aux[k]=arr[i++];
+         else if(aux[j]<aux[i]) aux[k]=arr[j++];
+         else arr[k]=aux[i++];
+	} 
+
+  }
+
+
+}

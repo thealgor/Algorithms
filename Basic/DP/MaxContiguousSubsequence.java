b3fcaeda212e190c944eaa9bdbfd2989e33dcd4a
Sunil <sunil.kksubramanyam@salesforce.com>
2015-01-30 00:10:14 -0800

    added Max contig subseq

--- Basic/DP/MaxContiguousSubsequence.java
+++ Basic/DP/MaxContiguousSubsequence.java
@@ -1,4 +1,30 @@
-public class MaxContiguousSubsequence{
+ipublic class MaxContiguousSubsequence{
 	
-	
+    public static void maxSubSeq(int[] arr){
+    	int start = -1, end = -1,gstart,gend, currMax = Integer.MIN_VALUE, globalMax = Integer.MIN_VALUE;
+    	for(int i=0;i<arr.length;i++){
+    		int v = arr[i];
+    		if(v> currMax+v){
+    			currMax = v;
+    			start = i;
+    			end = i;
+    		}
+    		else{
+    			currMax += v;
+    		    end = i; 	
+    		}
+    		if(currMax>globalMax){
+    			globalMax = currMax;
+    			gstart = start;
+    			gend = end;
+
+    		}
+    	}
+    	System.out.println("start:"+gstart+"end:"+gend+"maxsubseqsum:"+globalMax);
+    }
+	public static void main(String[] args){
+		int[] testArray = {6,-7,2,4,-6,3,2,5,-4};
+		maxSubSeq(testArray);
+	}
+
 }

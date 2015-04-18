ae64f3964fcf59f5f7ce2db79b33e0d6d0303b0e
Sunil <sunil.kksubramanyam@salesforce.com>
2015-04-18 12:25:05 -0700

    Added delete given node

---
+++ Basic/LinkedList/PointerToGivenNodeDelete.java
@@ -0,0 +1,20 @@
+public class PointerToGivenNodeDelete{
+	private static class Node{
+		private int data;
+		private Node next;
+		public Node(int data, Node next){
+			this.data =data;
+			this.next = next;
+		}
+	}
+	public static void deleteNode(Node ptr){
+		if(ptr==null || ptr.next==null)
+			throw new IllegalArgumentExcpetion("Send the node somewhwer in the middle of a linked list";)
+		Node nextNode = ptr.next;
+		int tempdata = nextNode.data;
+		ptr.data = tempdata;
+		ptr.next = nextNode.next;
+		nextNode.next=null;
+	}
+	//no testing needed I guess.
+}

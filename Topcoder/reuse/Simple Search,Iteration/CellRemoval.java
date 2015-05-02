c134b6e27fa6c600a0eceb7a55fa5a091749cb4a
Sunil <sunil.kksubramanyam@salesforce.com>
2015-05-02 13:47:32 -0700

    Added my version of cell removal, retry..missed some conditions

--- Topcoder/reuse/Simple Search,Iteration/CellRemoval.java
+++ Topcoder/reuse/Simple Search,Iteration/CellRemoval.java
@@ -47,6 +47,59 @@ public class CellRemoval
 	  }
 	}
 	
-	<%:testing-code%>
+	//<%:testing-code%>
+}
+//Powered by [KawigiEdit] 2.0!
+
+
+class CellRemoval1
+{
+	boolean[][] isConnected;
+	boolean[] marked;
+	boolean[] isLeaf;
+	int deleted;
+	int count = 0;
+	public int cellsLeft(int[] parent, int deletedCell)
+	{
+		isConnected = new boolean[parent.length][parent.length];
+		marked= new boolean[parent.length];
+		isLeaf = new boolean[parent.length];
+		deleted = deletedCell;
+		Arrays.fill(isLeaf,true);
+		int src=-1;
+		for(int i=0;i<parent.length;i++){
+			if(parent[i]==-1){
+				src = i;
+				continue;
+			}
+
+			isLeaf[parent[i]]=false;
+			isConnected[parent[i]][i]=true;
+			//isConnected[i][parent[i]]=true;
+
+		}
+		marked[deletedCell]=true;
+		if(src==deletedCell)//missed this condition was failing otherwise I have already entered into the dfs loop and started going a depth below
+			return 0;
+
+		dfs(src);
+		return count;
+	}
+
+	public void dfs(int src){
+		if(isLeaf[src]){
+			count++;
+		}
+		for(int i=0;i<isConnected[0].length;i++){
+			if(i==src)
+				continue;
+			if(!marked[i]&&isConnected[src][i]) { //made a mistake here marked[i]
+				marked[src] = true;
+				dfs(i);
+			}
+		}
+	}
+
+
 }
 //Powered by [KawigiEdit] 2.0!
